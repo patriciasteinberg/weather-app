66ab52b2104e581fff47fa9d0eced3b71e4c28b9
patriciasteinberg <patriciacsteinberg@gmail.com>
2019-05-21 19:41:34 -0300

    Requesting summary, temperature and chance of rain from API

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-const request = require('request');
+const request = require("request");
 
-const url = 'https://api.darksky.net/forecast/585669c0eaac2fbb8fb05cefe3cba272/37.8267,-122.4233';
+const url =
+    "https://api.darksky.net/forecast/585669c0eaac2fbb8fb05cefe3cba272/37.8267,-122.4233?unit=si";
 
-request({
-    url: url
-}, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.currently);
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.daily.data[0].summary + " It is currently " + response.body.currently.temperature + " degrees celsius out. There is a " + response.body.currently.precipProbability + " chance of rain.");
 });
